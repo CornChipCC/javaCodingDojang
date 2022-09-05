264e6554c5d6682df75213e5158fe1a5dd8c8b63
DongminKim <sgchfhr@naver.com>
2022-09-05 13:32:16 +0900

    Duplicate Numbers

--- src/practice9.java
+++ src/practice9.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
+/* Duplicate Numbers */
 public class practice9 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();        
+
+        int[] countNum = new int[10];
+        boolean answer = true;
+
+        for (int i = 0; i < input.length(); i++) {
+            int a = input.charAt(i)- '0';                   // char형을 int형으로
+            countNum[a]++;
+        }
+
+        for(int i = 0; i< countNum.length; i++) {
+            if(countNum[i] != 1){
+                answer = false;
+                break;
+            }
+        }
+        System.out.println(answer);
     }
 }
