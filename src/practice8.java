56f1c0e213b89eae1d0c31aaad62503ed70360f7
DongminKim <sgchfhr@naver.com>
2022-09-05 10:59:36 +0900

    메모리공간을 동적으로 사용하여 데이터 관리하기

---
+++ src/practice8.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class practice8 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("입력할 정수의 개수를 입력하세요 : ");
+        int num = sc.nextInt();
+        for(int i=1;i<=num;i++){
+            System.out.print("정수를 입력하세요 : ");
+            list.add(sc.nextInt());
+        }
+
+        int sum=0;
+
+        for(int i=0;i<list.size();i++){
+            sum+=list.get(i);
+        }
+
+        System.out.println("총합은 : " + sum);
+        System.out.printf("평균은 : %.2f" , (float)sum/num);
+        list.clear();
+    }
+}
