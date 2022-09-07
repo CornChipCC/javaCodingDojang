1ffaba382f16fd12edd40cce8dc92aae3d904fd5
DongminKim <sgchfhr@naver.com>
2022-09-07 16:45:28 +0900

    자릿수를 출력하는 프로그램

---
+++ src/practice25.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+/* 자릿수를 출력하는 프로그램 */
+public class practice25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println(str.length() + "자리수 입니다.");
+    }
+}
